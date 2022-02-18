c271e8d88f37e36eec6205db2477d94b08bc1a02
nickcastel <82051543+nickcastel@users.noreply.github.com>
2022-02-18 10:09:53 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,7 @@
-$("#response").html("scripts.js has loaded!");
+let userInput;
+userInput = "samosa";
+if ( userInput === "burrito" ) {
+  $("#response").html("Brilliant choice!");
+} else {
+  $("#response").html("Don’t you want a burrito?");
+}
