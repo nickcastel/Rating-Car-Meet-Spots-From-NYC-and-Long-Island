45c7900089f4788dd20eebd3021b598ce4cacdcb
nickcastel <82051543+nickcastel@users.noreply.github.com>
2022-03-25 09:56:39 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,7 +1,8 @@
-let userInput;
+/*let userInput;
 userInput = prompt("What do you want to have for dinner?", "Type your answer here.");
 if ( userInput === "burrito" ) {
   $("#response").html("Brilliant choice!");
 } else {
   $("#response").html("Don’t you want a burrito?");
 }
+*/
