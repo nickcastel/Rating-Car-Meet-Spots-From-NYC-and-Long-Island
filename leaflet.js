a499fb378d2384944e76dac21584938b49753aa3
nickcastel <82051543+nickcastel@users.noreply.github.com>
2022-03-25 09:44:17 -0400

    Update leaflet.js

--- leaflet.js
+++ leaflet.js
@@ -1,11 +1,11 @@
 let firstMap, tileLayer;
 firstMap = L.map("first-map");
-tileLayer =
+tileLayer =tileLayer.addTo(firstMap);
+firstMap.setView([40.730833, -73.9975], 16);
 L.tileLayer("https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png", {
       attribution: "&copy; <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> &copy; <a href='http://carto.com/attribution'>CARTO</a>",
       subdomains: "abcd",
       maxZoom: 18
       });
-  tileLayer.addTo(firstMap);
-firstMap.setView([40.730833, -73.9975], 16);
+
     
